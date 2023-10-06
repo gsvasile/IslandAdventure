548160a6876fd11986a8b8751f1f5ef6c5fb5ad3
Gabriel Vasile <g.vasile@gmail.com>
2023-10-06 15:59:49 -0700

    Refactored to initialize events to empty anonymous delegates.
    This makes it so the null check is not needed when the events are raised, there will always be one delegate added to the mutlicast delegate.

--- Assets/Scripts/Utility/BubbleEvent.cs
+++ Assets/Scripts/Utility/BubbleEvent.cs
@@ -5,48 +5,28 @@ namespace RPG.Utility
 {
     public class BubbleEvent : MonoBehaviour
     {
-        public event UnityAction OnBubbleStartAttack;
-        public event UnityAction OnBubbleCompleteAttack;
-        public event UnityAction OnBubbleHit;
-        public event UnityAction OnBubbleCompleteDefeat;
+        public event UnityAction OnBubbleStartAttack = () => { };
+        public event UnityAction OnBubbleCompleteAttack = () => { };
+        public event UnityAction OnBubbleHit = () => { };
+        public event UnityAction OnBubbleCompleteDefeat = () => { };
 
         private void OnStartAttack()
         {
-            if (OnBubbleStartAttack == null)
-            {
-                return;
-            }
-
             OnBubbleStartAttack.Invoke();
         }
 
         private void OnCompleteAttack()
         {
-            if (OnBubbleCompleteAttack == null)
-            {
-                return;
-            }
-
             OnBubbleCompleteAttack.Invoke();
         }
 
         private void OnHit()
         {
-            if (OnBubbleHit == null)
-            {
-                return;
-            }
-
             OnBubbleHit.Invoke();
         }
 
         private void OnCompleteDefeat()
         {
-            if (OnBubbleCompleteDefeat == null)
-            {
-                return;
-            }
-
             OnBubbleCompleteDefeat.Invoke();
         }
     }
