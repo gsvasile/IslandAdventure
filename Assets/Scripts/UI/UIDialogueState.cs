d347bfaad4f08add4b4306e7925d4d71ea6fb72f
Gabriel Vasile <g.vasile@gmail.com>
2023-10-10 23:14:12 -0700

    Added a dialogue area on the main menu UI.
    Initial check-in of the UI dialogue state.

---
+++ Assets/Scripts/UI/UIDialogueState.cs
@@ -0,0 +1,26 @@
+using UnityEngine.UIElements;
+
+namespace RPG.UI
+{
+    public class UIDialogueState : UIBaseState
+    {
+        private VisualElement dialogueContainer;
+        private Label dialogueText;
+        private VisualElement nextButton;
+        private VisualElement choicesGroup;
+
+        public UIDialogueState(UIController ui) : base(ui) { }
+
+        public override void EnterState()
+        {
+            dialogueContainer = Controller.Root.Q<VisualElement>("dialogue-container");
+            dialogueText = dialogueContainer.Q<Label>("dialogue-text");
+            nextButton = dialogueContainer.Q<VisualElement>("dialogue-next-button");
+            choicesGroup = dialogueContainer.Q<VisualElement>("choices-group");
+
+            dialogueContainer.style.display = DisplayStyle.Flex;
+        }
+
+        public override void SelectButton() { }
+    }
+}
